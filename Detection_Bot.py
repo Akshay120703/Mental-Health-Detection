dd47bf4e1cbd8d00221f95cf4ceb9eb9c433d4b1
Akshay Pophali <akshaypophali2003@gmail.com>
2023-07-05 14:17:57 +0530

    Update Detection_Bot.py

--- Detection_Bot.py
+++ Detection_Bot.py
@@ -1,4 +1,4 @@
-#Mental Health Detection
+#new version(version 3.0)
 import pandas as pd
 
 from google.colab import files
@@ -22,7 +22,20 @@ def chatbot():
         answers.append(answer.lower())
 
     # Count the number of "no" answers
-    num_no = answers.count("no")
+    num_no = answers.count("yes")
+
+    # Determine the stage of illness
+    stage = ""
+    if num_no < 7:
+        stage = "Stage 1"
+    elif num_no < 12:
+        stage = "Stage 2"
+    elif num_no < 17:
+        stage = "Stage 3"
+    elif num_no < 20:
+        stage = "Stage 4"
+    else:
+        stage = "Stage 5"
 
     # Check the 14th question to calculate Positivity
     if answers[13] == "yes":
@@ -37,12 +50,12 @@ def chatbot():
     output = dataset.iloc[:, 24][dataset.iloc[:, :24].eq(input_data.loc[0]).all(axis=1)]
 
     if len(output) > 0:
-        print("Chatbot output: You are suffering from ", output.values[0])
+        print("\nChatbot output:", output.values[0])
     else:
-        print("No matching output found.")
+        print("\nChatbot output: Stress")
 
     print("Positivity:", positivity)
-    print("%")
+    print("Stage of illness:", stage)
 
 # Run the chatbot
 chatbot()
